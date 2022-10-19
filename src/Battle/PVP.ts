620393355d879accd532218911c1447887ab6221
Bruno Cabral <bruno.cabral.silva2018@gmail.com>
2022-10-18 21:02:39 -0300

    requisito 10 em construção

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,8 @@
+// import Battle from './Battle';
+// import Fighter from '../Fighter';
+
+// export default class PVP extends Battle {
+//   constructor(player: Fighter) {
+//     super(player);
+//   }
+// }
